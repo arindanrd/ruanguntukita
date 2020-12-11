025477ee14260e86a46fbd2267d6860d5f243439
arindanrd12 <arindanrd@gmail.com>
2020-12-11 20:00:59 +0700

    webgis bella

---
+++ data/rth_cilacap_kota_6.js
@@ -0,0 +1,13 @@
+var json_rth_cilacap_kota_6 = {
+"type": "FeatureCollection",
+"name": "rth_cilacap_kota_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Alun-alun Cilacap", "jenis_rth": "Lapangan", "fasilitas": "Spot Foto, Lapangan, Kuliner", "vegetasi": "Rumput", "foto": "C:\\Users\\ACER\\Downloads\\Alun-Alun_Cilacap (1).jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.009699933050328, -7.727304574951131 ] } },
+{ "type": "Feature", "properties": { "gid": 2.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Taman Lapangan Batalion", "jenis_rth": "Lapangan", "fasilitas": "Lapangan", "vegetasi": "Rumput", "foto": "C:\\Users\\ACER\\Downloads\\WhatsApp-Image-2020-01-20-at-16.42.25-1024x515.jpeg" }, "geometry": { "type": "Point", "coordinates": [ 109.011956429532475, -7.726900373714844 ] } },
+{ "type": "Feature", "properties": { "gid": 3.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Pantai Teluk Penyu", "jenis_rth": "Pantai", "fasilitas": "Sewa Perahu, Spot Foto, Kuliner", "vegetasi": "Pohon", "foto": "C:\\Users\\ACER\\Downloads\\maxresdefault.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.019767898643877, -7.74235405399638 ] } },
+{ "type": "Feature", "properties": { "gid": 4.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Taman Rinjani", "jenis_rth": "Taman Kota", "fasilitas": "Taman bermain", "vegetasi": "Pohon", "foto": "C:\\Users\\ACER\\Downloads\\Taman-Kota-Kurang-Gazebo.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.016072528885616, -7.699911175290009 ] } },
+{ "type": "Feature", "properties": { "gid": 5.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Stadion Wijaya Kusuma", "jenis_rth": "Lapangan", "fasilitas": "Lapangan olahraga, Bangku Penonton", "vegetasi": "Rumput", "foto": "C:\\Users\\ACER\\Downloads\\Stadion-Wijayakusuma-Tidak-Untuk-Kampanye.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.034335550643448, -7.695450405312203 ] } },
+{ "type": "Feature", "properties": { "gid": 6.0, "waktu": "2020\/12\/06 00:00:00", "nama_rth": "Segara Anakan", "jenis_rth": "Pantai", "fasilitas": "Spot Foto, Wisata Alam", "vegetasi": "Pohon", "foto": "C:\\Users\\ACER\\Downloads\\segara-anakan.jpg" }, "geometry": { "type": "Point", "coordinates": [ 109.010939865728503, -7.663401691752044 ] } }
+]
+}
---
+++ data/tanam_2.js
@@ -0,0 +1,8 @@
+var json_tanam_2 = {
+"type": "FeatureCollection",
+"name": "tanam_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "gid": 1.0, "fid_agrita": "0", "namobj": null, "remark": "Vegetasi Non Budidaya Lainnya", "aqdate": null, "pudate": null, "fcode": null, "kodlco": null, "srs_id": null, "lcode": "FG0020", "metadata": null, "jnsswh": "0", "shape_leng": 0.0125293443813, "shape_area": 1.7036642488e-06, "fid_cilaca": "0", "kdppum": null, "namobj_1": "Tritih Kulon", "remark_1": "Wilayah Administrasi Keluruhan\/Desa", "kdpbps": null, "fcode_1": null, "luaswh": 0.0, "uupp": null, "srs_id_1": null, "lcode_1": "BA0020", "metadata_1": null, "kdebps": null, "kdepum": null, "kdcbps": null, "kdcpum": null, "kdbbps": null, "kdbpum": null, "wadmkd": null, "wiadkd": null, "wadmkc": "Cilacap Utara", "wiadkc": null, "wadmkk": "Kabupaten Cilacap", "wiadkk": null, "wadmpr": "Jawa Tengah", "wiadpr": null, "tipadm": "0", "shape_le_1": 0.17940283937599999, "shape_ar_1": 0.0010233733949200001 }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 109.033795976000064, -7.644728975, 0.0 ], [ 109.033606026000072, -7.64492696199995, 0.0 ], [ 109.029221034000074, -7.640873992999982, 0.0 ], [ 109.029412975000056, -7.640648992000017, 0.0 ], [ 109.033795976000064, -7.644728975, 0.0 ] ] ] ] } }
+]
+}
